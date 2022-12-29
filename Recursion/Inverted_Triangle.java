d1c4103d0f029a8aea2c8bc9aa497e2817c6a406
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2022-12-29 10:01:28 +0530

    Add files via upload

---
+++ Recursion/Inverted_Triangle.java
@@ -0,0 +1,49 @@
+/*
+Take as input N, a number. Print the following pattern (for N = 5)
+
+       *    *   *   *   *   
+       *    *   *   *
+       *    *   *
+       *    *   
+       *    
+Input Format
+Enter value of N
+
+Constraints
+None
+
+Output Format
+Tab separated (*)
+
+Sample Input
+5
+Sample Output
+*	*	*	*	*	
+*	*	*	*	
+*	*	*	
+*	*	
+*	
+
+ */
+package Recursion;
+
+import java.util.Scanner;
+
+public class Inverted_Triangle {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		inverted_Triangle(n);
+	}
+
+	public static void inverted_Triangle(int n) {
+		if (n == 0) {
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			System.out.print("*\t");
+		}
+		System.out.println();
+		inverted_Triangle(n - 1);
+	}
+}
