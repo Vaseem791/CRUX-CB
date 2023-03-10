fa805dff8b444531137234acd3213408c30a31e7
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-10 17:37:55 +0530

    Add files via upload

---
+++ Arrays/Maximum_Circular_Sum.java
@@ -0,0 +1,78 @@
+/*You are provided n numbers (both +ve and -ve). Numbers are arranged in a circular form. You need to find the maximum sum of consecutive numbers.
+
+Input Format
+First line contains integer t which is number of test case.
+For each test case, it contains an integer n which is the size of array and next line contains n space separated integers denoting the elements of the array.
+
+Constraints
+1<=t<=100
+1<=n<=1000
+|Ai| <= 10000
+
+Output Format
+Print the maximum circular sum for each testcase in a new line.
+
+Sample Input
+1
+7
+8 -8 9 -9 10 -11 12
+Sample Output
+22
+Explanation
+Maximum Circular Sum = 22 (12 + 8 - 8 + 9 - 9 + 10)
+*/
+package Arrays;
+
+import java.util.*;
+
+public class Maximum_Circular_Sum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- != 0) {
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = sc.nextInt();
+			}
+
+			System.out.println(MaximumSum(arr));
+		}
+
+	}
+
+	public static int MaximumSum(int[] arr) {
+		int linear_sum = kadens(arr);
+		int totalsum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			totalsum = totalsum + arr[i];
+			arr[i] = arr[i] * -1;
+
+		}
+		int mid_sum = kadens(arr);
+
+		int curr_sum = totalsum + mid_sum;
+		if (curr_sum == 0) {
+			return linear_sum;
+		}
+
+		return Math.max(linear_sum, curr_sum);
+
+	}
+
+	public static int kadens(int[] arr) {
+
+		int ans = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+			ans = Math.max(ans, sum);
+			if (sum < 0) {
+				sum = 0;
+			}
+		}
+		return ans;
+
+	}
+
+}
---
+++ Arrays/Maximum_Sum_Path_in_Two_Arrays.java
@@ -0,0 +1,95 @@
+/*You are provided two sorted arrays. You need to find the maximum length of bitonic subsequence. You need to find the sum of the maximum sum path to reach from beginning of any array to end of any of the two arrays. You can switch from one array to another array only at common elements.
+
+Input Format
+First line contains integer t which is number of test case. For each test case, it contains two integers n and m which is the size of arrays and next two lines contains n and m space separated integers respectively.
+
+Constraints
+1<=t<=100 1<=n,m<=100000
+
+Output Format
+Print the maximum path.
+
+Sample Input
+1
+8 8
+2 3 7 10 12 15 30 34
+1 5 7 8 10 15 16 19
+Sample Output
+122
+
+Explanation
+122 is sum of 1, 5, 7, 8, 10, 12, 15, 30, 34
+
+*/
+
+package Arrays;
+
+import java.util.*;
+
+public class Maximum_Sum_Path_in_Two_Arrays {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int[] arr = new int[n];
+			int[] brr = new int[m];
+			for (int i = 0; i < arr.length; i++) {
+
+				arr[i] = sc.nextInt();
+			}
+			for (int i = 0; i < brr.length; i++) {
+				brr[i] = sc.nextInt();
+			}
+			System.out.println(pathSum(arr, brr));
+		}
+
+	}
+
+	public static int pathSum(int[] arr, int[] brr) {
+		int i = 0;// arr
+		int j = 0;// brr
+		int p = 0;// arr start
+		int q = 0;// brr start
+		int ans = 0;
+		while (i < arr.length && j < brr.length) {
+			if (arr[i] < brr[j]) {
+				i++;
+			} else if (arr[i] > brr[j]) {
+				j++;
+			} else {
+				int sum1 = 0;
+				int sum2 = 0;
+				for (int k = p; k <= i; k++) {
+					sum1 = sum1 + arr[k];
+
+				}
+				for (int k = q; k <= j; k++) {
+					sum2 += brr[k];
+
+				}
+				ans = ans + Math.max(sum1, sum2);
+				i++;
+				j++;
+				p = i;
+				q = j;
+			}
+		}
+
+		if (i == arr.length) {
+			for (int k = q; k < brr.length; k++) {
+				ans = ans + brr[k];
+			}
+		}
+
+		if (j == brr.length) {
+			for (int k = p; k < arr.length; k++) {
+				ans = ans + arr[k];
+			}
+		}
+		return ans;
+
+	}
+}
