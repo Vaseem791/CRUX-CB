d31d0de23f57fb934c553fddd8244ff6a7293da4
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 09:25:53 +0530

    Update String_Remove_Duplicates.java

--- Strings/String_Remove_Duplicates.java
+++ Strings/String_Remove_Duplicates.java
@@ -25,7 +25,7 @@ package Strings;
 
 import java.util.*;
 
-public class String_Remove_Duplicates {
+public class Main {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		String S = scn.nextLine();
