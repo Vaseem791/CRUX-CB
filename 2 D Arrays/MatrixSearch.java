eaf7772c7f13eee7812692ed519dafd3827d6e89
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 08:09:42 +0530

    Update MatrixSearch.java

--- 2 D Arrays/MatrixSearch.java
+++ 2 D Arrays/MatrixSearch.java
@@ -1,7 +1,9 @@
 /*Given an n x m matrix, where every row and column is sorted in increasing order, and a number x . Find if element x is present in the matrix or not.
 
 Input Format
-First line consists of two space separated integers N and M, denoting the number of element in a row and column respectively. Second line of each test case consists of N*M space separated integers denoting the elements in the matrix in row major order. Third line of each test case contains a single integer x, the element to be searched.
+First line consists of two space separated integers N and M, denoting the number of element in a row and column respectively. 
+Second line of each test case consists of N*M space separated integers denoting the elements in the matrix in row major order. T
+hird line of each test case contains a single integer x, the element to be searched.
 
 Constraints
 1 <= N,M <= 30 0 <= A[i] <= 100
@@ -18,12 +20,14 @@ Sample Input
 Sample Output
 0
 Explanation
-Search the element in the sorted matrix. If the element is present print 1 otherwise print 0. In the sample input,in first case 62 is not present in the matrix so 0 is printed. Similarly, for second case 55 is present in the matrix so 1 is printed.*/
+Search the element in the sorted matrix. If the element is present print 1 otherwise print 0. 
+In the sample input,in first case 62 is not present in the matrix so 0 is printed. Similarly, for second case 55 is present in the matrix so 1 is printed.*/
+
 package Two_D_Arrays;
 
 import java.util.*;
 
-public class MatrixSearch {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int row = sc.nextInt();
