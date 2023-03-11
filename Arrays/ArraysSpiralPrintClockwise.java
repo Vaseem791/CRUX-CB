f3ef79e069e1119838f0c87bb46b3e9054e67759
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 08:21:18 +0530

    Update ArraysSpiralPrintClockwise.java

--- Arrays/ArraysSpiralPrintClockwise.java
+++ Arrays/ArraysSpiralPrintClockwise.java
@@ -23,7 +23,7 @@ package Arrays;
 
 import java.util.Scanner;
 
-public class ArraysSpiralPrintClockwise {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int row = sc.nextInt();
