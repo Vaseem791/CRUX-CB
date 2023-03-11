fe10c7e7c22bbcaeac95dacce3849a2d8cd9ba23
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 07:59:55 +0530

    Add files via upload

---
+++ Sorting/Maximum_Integer_With_At_Most_K_Swaps.java
@@ -0,0 +1,59 @@
+/*Given a positive integer, find maximum integer possible by doing at-most K swap operations on its digits.
+
+Input Format
+Two positive integers N and K where N denotes the number on which swaps are supposed to be performed and K denotes the maximum swaps available.
+
+Constraints
+0 <= N <= 10^12 0 <= K <= 10
+
+Output Format
+Single Maximum integer possible by doing at-most K swap operations on digits of N
+
+Sample Input
+254 1
+
+Sample Output
+524
+
+*/
+package Sorting;
+
+import java.util.Scanner;
+
+public class Maximum_Integer_With_At_Most_K_Swaps {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		String str = scn.next();;
+		int k = scn.nextInt();
+		max = str;
+		findMaximumNum(str.toCharArray(), k);
+		System.out.print(max + "\n");
+	}
+
+	static String max;
+
+	public static void findMaximumNum(char[] str, int k) {
+		if (k == 0)
+			return;
+		int n = str.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (str[i] < str[j]) {
+					char t = str[i];
+					str[i] = str[j];
+					str[j] = t;
+					
+					if (String.valueOf(str).compareTo(max) > 0)
+						max = String.valueOf(str);
+					findMaximumNum(str, k - 1);
+
+					// Backtrack
+					char c = str[i];
+					str[i] = str[j];
+					str[j] = c;
+				}
+			}
+		}
+	}
+}
