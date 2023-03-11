c6b2b54d5cc6cc05ff98ed7e0b72e7a421250e8a
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 16:00:40 +0530

    Add files via upload

---
+++ Strings/Playing_With_Good_String.java
@@ -0,0 +1,50 @@
+/*A Good String is a string which contains only vowels (a,e,i,o,u).Given a string S,print a single
+  positive integer N where N is the length of the longest substring of S that is also a Good String.
+
+Note: The time limit for this problem is 1 second,so you need to be clever in how you compute the substrings.
+
+Input Format
+A string 'S'
+
+Constraints
+Length of string < 10^5
+
+Output Format
+A single positive integer N, where N is the length of the longest sub-string of S that is also a Good String.
+
+Sample Input
+cbaeicde
+
+Sample Output
+3
+
+Explanation
+Longest good substring is "aei"
+*/
+package Strings;
+
+import java.util.*;
+
+public class Playing_With_Good_String {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String S = scn.next();
+		System.out.println(GoodString(S));
+	}
+
+	public static int GoodString(String S) {
+		int length = 0;
+		int ans = 0;
+		for (int i = 0; i < S.length(); i++) {
+			char ch = S.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				length++;
+			} else {
+				length = 0;
+			}
+			ans = Math.max(ans, length);
+		}
+		return ans;
+	}
+}
