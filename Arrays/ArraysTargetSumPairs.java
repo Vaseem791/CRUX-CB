2eaa92b93aaccdd21378794c6c3b47e90ae66047
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 08:22:14 +0530

    Update ArraysTargetSumPairs.java

--- Arrays/ArraysTargetSumPairs.java
+++ Arrays/ArraysTargetSumPairs.java
@@ -1,4 +1,5 @@
-/*Take as input N, the size of array. Take N more inputs and store that in an array. Take as input “target”, a number. Write a function which prints all pairs of numbers which sum to target.
+/*Take as input N, the size of array. Take N more inputs and store that in an array. Take as input “target”, a number. 
+Write a function which prints all pairs of numbers which sum to target.
 
 Input Format
 The first line contains input N. Next N lines contains the elements of array and (N+1)th line contains target number.
@@ -17,11 +18,14 @@ Sample Input
 2
 5
 5
+
 Sample Output
 1 and 4
 2 and 3
+
 Explanation
 Find any pair of elements in the array which has sum equal to target element and print them.*/
+
 package Arrays;
 
 import java.util.*;
