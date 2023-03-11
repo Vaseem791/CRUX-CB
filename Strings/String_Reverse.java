810ce39677831e0b3c335d886124d4569ce81673
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 09:26:25 +0530

    Update String_Reverse.java

--- Strings/String_Reverse.java
+++ Strings/String_Reverse.java
@@ -22,7 +22,7 @@ package Strings;
 
 import java.util.Scanner;
 
-public class Mian {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String r = sc.nextLine();
