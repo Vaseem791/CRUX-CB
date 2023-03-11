31080b74d97fe517d7b8290bf8c560feb328e397
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 08:14:06 +0530

    Update Triangle_Pattern_1.java

--- Recursion/Triangle_Pattern_1.java
+++ Recursion/Triangle_Pattern_1.java
@@ -39,7 +39,7 @@ Sample Output
 package Recursion;
 import java.util.Scanner;
 
-public class Triangle_Pattern_1 {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
