22a6642a37e75fdcd729399979248c98f836dcfc
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-11 08:03:32 +0530

    Update Maximum_Integer_With_At_Most_K_Swaps.java

--- Sorting/Maximum_Integer_With_At_Most_K_Swaps.java
+++ Sorting/Maximum_Integer_With_At_Most_K_Swaps.java
@@ -20,7 +20,7 @@ package Sorting;
 
 import java.util.Scanner;
 
-public class Maximum_Integer_With_At_Most_K_Swaps {
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner scn=new Scanner(System.in);
@@ -34,8 +34,9 @@ public class Maximum_Integer_With_At_Most_K_Swaps {
 	static String max;
 
 	public static void findMaximumNum(char[] str, int k) {
-		if (k == 0)
+		if (k == 0){
 			return;
+		}
 		int n = str.length;
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = i + 1; j < n; j++) {
@@ -44,8 +45,9 @@ public class Maximum_Integer_With_At_Most_K_Swaps {
 					str[i] = str[j];
 					str[j] = t;
 					
-					if (String.valueOf(str).compareTo(max) > 0)
+					if (String.valueOf(str).compareTo(max) > 0){
 						max = String.valueOf(str);
+					}
 					findMaximumNum(str, k - 1);
 
 					// Backtrack
