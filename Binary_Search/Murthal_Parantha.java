a3d93a4cd8b29dc9e4f6923763a348cdc79d7a9a
Vaseem Ali <116418752+Vaseem791@users.noreply.github.com>
2023-03-16 12:18:59 +0530

    Update Murthal_Parantha.java

--- Binary_Search/Murthal_Parantha.java
+++ Binary_Search/Murthal_Parantha.java
@@ -1,8 +1,8 @@
-/*The coding blocks members went to the success party of their first ever online boot-camp at Murthal. They ordered P number of paranthas. 
- * The stall has L cooks and each cook has a rank R. A cook with a rank R can cook 1 parantha in the first R minutes 1 more parantha in the next 2R minutes,
- *  1 more parantha in 3R minutes and so on(he can only cook a complete parantha) ( For example if a cook is ranked 2.. he will cook one parantha in 2 minutes one more parantha in the next 4 mins 
- *  and one more in the next 6 minutes hence in total 12 minutes he cooks 3 paranthas. In 13 minutes also he can cook only 3 paranthas as he does not have enough time for the 4th parantha). 
- *  Calculate the minimum time needed to cook all the paranthas.
+/*The coding blocks members went to the success party of their first ever online boot-camp at Murthal. They ordered P number of paranthas.  The stall has L cooks and
+ each cook has a rank R. A cook with a rank R can cook 1 parantha in the first R minutes 1 more parantha in the next 2R minutes, 1 more parantha in 3R    minutes and
+ so on(he can only cook a complete parantha) ( For example if a cook is ranked 2.. he will cook one parantha in 2 minutes one more parantha in the next 4 mins 
+ and one more in the next 6 minutes hence in total 12 minutes he cooks 3 paranthas. In 13 minutes also he can cook only 3 paranthas as he does not have enough time for the 4th parantha). 
+ Calculate the minimum time needed to cook all the paranthas.
 
 Input Format
 First line contains P, the number of pratha ordered. In the next line the first integer denotes the number of cooks L 
@@ -29,11 +29,11 @@ Third cook with rank 3 cooks 2 paranthas in 9 minutes (3+6) Fourth cook with ran
  He can do that in 4 minutes.
 Since these cooks cook parallely, the total time taken will be the maximum of the four i.e. 12 minutes.
 */
-package Binary_Search;
+
 
 import java.util.Scanner;
 
-public class Murthal_Parantha {
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
